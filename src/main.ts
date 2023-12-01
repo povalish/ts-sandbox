fe1840988de00e58dfb1452afa241c8315694e06
Nikita Povalishev <np@skl.vc>
2023-12-01 21:22:18 +0300

    Create script for downloading images

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,97 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import fs from 'fs-extra';
+import https from 'https';
+
+
+//
+// Types
+
+type ImageUrlParams = {
+  id: string;
+  size: string;
+  ext: string;
+};
+
+type GameData = {
+  identifier: string;
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+type GameResponse = {
+  data: GameData[];
+  pagination: { next_page: number };
+};
+
+//
+// Utils
+
+const getImagePath = ({ id, size, ext }: ImageUrlParams) => {
+  return `i/${size}/${id}.${ext}`;
+};
+
+const getImageUrl = (path: string) => {
+  return `https://cdn2.softswiss.net/${path}`;
+};
+
+const createWriteStream = (imagePath: string) => {
+  const path = imagePath.split('/');
+  const fileName = path.pop();
+  const dir = path.join('/');
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  return fs.createWriteStream(imagePath);
+};
+
+//
+// Requests
+
+const downloadAndSaveFile = ({ id, size, ext }: ImageUrlParams) => {
+  const imagePath = getImagePath({ id, size, ext });
+  const imageUrl = getImageUrl(imagePath);
+  const file = createWriteStream(`./build/download-data/${imagePath}`);
+
+  https.get(imageUrl, (response) => {
+    console.log('Downloaded file: ', getImagePath({ id, size, ext }));
+    response.pipe(file);
+
+    file.on('finish', () => {
+      file.close();
+      console.log('File saved: ', getImagePath({ id, size, ext }));
+    });
+  });
+};
+
+const getGames = async () => {
+  const result: GameData[] = [];
+
+  let nextPage = 4;
+
+  // while (nextPage) {
+    const response = await fetch('https://ethplay.io/api/games_filter', {
+      method: 'POST',
+      body: JSON.stringify({ page: 1, page_size: 10 }),
+    });
+
+    
+    const parsedResponse = await response.json() as GameResponse;
+    
+    result.push(...parsedResponse.data);
+    nextPage = parsedResponse.pagination.next_page;
+  // }
+
+  return result;
+};
+
+export async function main() {
+  const allGames = await getGames();
+  allGames.forEach((game) => {
+    downloadAndSaveFile({
+      ext: 'webp',
+      id: game.identifier,
+      size: 's2',
+    });
+  });
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+await main();
